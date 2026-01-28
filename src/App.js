760ca0b2595930324bed830ef6dfcf7f5c8523d3
Arseniy <144793918+Arseniypir231@users.noreply.github.com>
2026-01-28 15:41:07 +0300

    refactor: integrate AppContext and new routes, add ToastContainer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AppProvider } from './context/AppContext';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Recipes from './pages/Recipes';
 import Article from './pages/Article';
 import Contact from './pages/Contact';
+import Search from './pages/Search';
+import Favorites from './pages/Favorites';
+import About from './pages/About';
 import menuData from './data/menu.json';
 import appConfigData from './data/appConfig.json';
 import './App.css';
@@ -24,31 +30,48 @@ const App = () => {
     }
 
     return (
-        <Router>
-            <div className="App">
-                <article className="fashion">
-                    <Header 
+        <AppProvider>
+            <Router>
+                <div className="App">
+                    <article className="fashion">
+                        <Header 
+                            organizationName={appConfig.organizationName}
+                            menuItems={menuItems}
+                        />
+                    </article>
+                    <main>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/recipes" element={<Recipes />} />
+                            <Route path="/article" element={<Article />} />
+                            <Route path="/article/:id" element={<Article />} />
+                            <Route path="/contact" element={<Contact />} />
+                            <Route path="/search" element={<Search />} />
+                            <Route path="/favorites" element={<Favorites />} />
+                            <Route path="/about" element={<About />} />
+                        </Routes>
+                    </main>
+                    <Footer 
                         organizationName={appConfig.organizationName}
                         menuItems={menuItems}
+                        socialIcons={appConfig.socialIcons}
+                        copyrightText={appConfig.copyrightText}
                     />
-                </article>
-                <main>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/recipes" element={<Recipes />} />
-                        <Route path="/article" element={<Article />} />
-                        <Route path="/article/:id" element={<Article />} />
-                        <Route path="/contact" element={<Contact />} />
-                    </Routes>
-                </main>
-                <Footer 
-                    organizationName={appConfig.organizationName}
-                    menuItems={menuItems}
-                    socialIcons={appConfig.socialIcons}
-                    copyrightText={appConfig.copyrightText}
-                />
-            </div>
-        </Router>
+                    <ToastContainer
+                        position="top-right"
+                        autoClose={3000}
+                        hideProgressBar={false}
+                        newestOnTop={false}
+                        closeOnClick
+                        rtl={false}
+                        pauseOnFocusLoss
+                        draggable
+                        pauseOnHover
+                        theme="light"
+                    />
+                </div>
+            </Router>
+        </AppProvider>
     );
 };
 
